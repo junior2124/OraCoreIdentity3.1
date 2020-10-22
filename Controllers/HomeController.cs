f2fc0ebcd2523cfb79686984ec6a229d7b1f9c5c
EmilioEscobedo <emilio.escobedo@apci.org>
2020-10-22 16:10:18 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OraCoreIdentity3._1.Models;
+using OraCoreIdentity3._1.Data;
+
+namespace OraCoreIdentity3._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ApplicationDbContext context, ILogger<HomeController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            var addresses = _context.COMPANYADDRESS.ToList();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OraCoreIdentity3.Models;
+
+namespace OraCoreIdentity3._1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<COMPANYADDRESS> COMPANYADDRESS { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<COMPANYADDRESS>(entity =>
+            {
+                entity.ToTable("COMPANYADDRESS", "NAII");
+                entity.HasKey(e => new { e.COMPANYID, e.MAINADDRESSESID });
+            });
+
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
